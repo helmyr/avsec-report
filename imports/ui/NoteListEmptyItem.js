1e0a6f9ca2bec77b33d10d6654770cb1a4dc5982
Helmy R <helmy87@gmail.com>
2018-06-26 23:39:32 +0800

    Init commit

---
+++ imports/ui/NoteListEmptyItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NoteListEmptyItem = () => {
+  return (
+    <p className="empty-item">Create a report to get started!</p>
+  );
+};
+
+export default NoteListEmptyItem;
